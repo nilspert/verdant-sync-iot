ce7bfd3f60ef7eb454ac40e763de392d8a99a10a
Joonas Nislin <joonas.nislin@hotmail.com>
2023-08-18 15:54:26 +0300

    ML-9: Enhance Code Efficiency and Functionality
    - Optimize codebase for improved efficiency, readability, and maintainability.
    - Refactor WiFi initialization, AES encryption, and Firebase data transmission.
    - Enhance board information retrieval and added dummy temperature data transmission processes.
    - Improve overall code structure and organization.
    
    Closes #9

--- VerdantSyncIot/VerdantSyncIot.ino
+++ VerdantSyncIot/VerdantSyncIot.ino
@@ -7,12 +7,15 @@
 
 AESLib aesLib;
 FirebaseData firebaseData;
-bool boardInfoSent = false; // Flag to track if board info has been sent
 
 // AESLib related variables
 unsigned char ciphertext[2*INPUT_BUFFER_LIMIT] = {0}; // THIS IS OUTPUT BUFFER (FOR BASE64-ENCODED ENCRYPTED DATA)
 byte aes_key[16]; // AES Encryption Key
 byte enc_iv[N_BLOCK]; // General initialization vector
+byte enc_iv_1[N_BLOCK];
+byte enc_iv_2[N_BLOCK];
+byte enc_iv_3[N_BLOCK];
+byte enc_iv_4[N_BLOCK];
 
 // Initialize and connect to WiFi
 void wifi_init() {
@@ -29,16 +32,14 @@ void aes_init() {
     aesLib.set_paddingmode((paddingMode)0);
     memcpy(aes_key, ENCRYPTION_SECRET_KEY, sizeof(aes_key));
     memcpy(enc_iv, ENCRYPTION_SECRET_KEY_IV, sizeof(enc_iv));
+    memcpy(enc_iv_1, ENCRYPTION_SECRET_KEY_IV, sizeof(enc_iv_1));
+    memcpy(enc_iv_2, ENCRYPTION_SECRET_KEY_IV, sizeof(enc_iv_2));
+    memcpy(enc_iv_3, ENCRYPTION_SECRET_KEY_IV, sizeof(enc_iv_3));
+    memcpy(enc_iv_4, ENCRYPTION_SECRET_KEY_IV, sizeof(enc_iv_4));
 }
 
-void encryptAndConvertToHex(const char* data, char* encryptedData) {
-    Serial.println("Encrypting and converting to hex string...");
-    uint16_t dataLength = strlen(data);
-    int cipherLength = aesLib.encrypt((byte*)data, dataLength, (byte*)ciphertext, aes_key, sizeof(aes_key), enc_iv);
-    
-    for (int i = 0; i < cipherLength; i++) {
-        sprintf(encryptedData + (2 * i), "%02X", ciphertext[i]);
-    }
+void init_firebase() {
+   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
 }
 
 // Called only once
@@ -47,16 +48,41 @@ void setup() {
   
     wifi_init(); // Call WiFi init function
     aes_init(); // Call AESLib init function
+    init_firebase(); // Call Firebase init function
 
-    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+    sendBoardInfo();
+}
+
+void sendFirebaseData(FirebaseJson json, const char* nodePath) {
+    if (Firebase.updateNode(firebaseData, nodePath, json)) {
+        Serial.println("Data added to Firebase successfully!");
+    } else {
+        Serial.print("Error adding data to Firebase: ");
+        Serial.println(firebaseData.errorReason());
+    }
+}
+
+void generateNewIV(byte destinationIV[], const byte sourceIV[]) {
+    memcpy(destinationIV, sourceIV, N_BLOCK);
+    // Modify the IV here if needed
+}
+
+void encryptAndConvertToHex(const char* data, char* encryptedData, byte iv[]) {
+    Serial.println("Encrypting and converting to hex string...");
+    uint16_t dataLength = strlen(data);
+    int cipherLength = aesLib.encrypt((byte*)data, dataLength, (byte*)ciphertext, aes_key, sizeof(aes_key), iv);
+    
+    for (int i = 0; i < cipherLength; i++) {
+        sprintf(encryptedData + (2 * i), "%02X", ciphertext[i]);
+    }
 }
 
 // Function to gather and encrypt board information
-void gatherAndEncryptBoardInfo(char* encryptedBoardName, char* encryptedFirmwareVersion, char* encryptedIpAddress, char* encryptedWifiSSID) {
-    encryptAndConvertToHex(BOARD_NAME, encryptedBoardName);
-    encryptAndConvertToHex(FIRMWARE_VERSION, encryptedFirmwareVersion);
-    encryptAndConvertToHex(WiFi.localIP().toString().c_str(), encryptedIpAddress);
-    encryptAndConvertToHex(WiFi.SSID().c_str(), encryptedWifiSSID);
+void gatherAndEncryptBoardInfo(char* encryptedFirmwareVersion, char* encryptedBoardName, char* encryptedIpAddress, char* encryptedWifiSSID) {
+    encryptAndConvertToHex(FIRMWARE_VERSION, encryptedFirmwareVersion, enc_iv);
+    encryptAndConvertToHex(BOARD_NAME, encryptedBoardName, enc_iv_1);
+    encryptAndConvertToHex(WiFi.localIP().toString().c_str(), encryptedIpAddress, enc_iv_2);
+    encryptAndConvertToHex(WiFi.SSID().c_str(), encryptedWifiSSID, enc_iv_3);
 }
 
 // Function to send board information to Firebase
@@ -65,11 +91,13 @@ void sendBoardInfo() {
     String boardId = WiFi.macAddress();
 
     // Gather and encrypt board information
-    char encryptedBoardName[INPUT_BUFFER_LIMIT] = {0};
     char encryptedFirmwareVersion[INPUT_BUFFER_LIMIT] = {0};
+    char encryptedBoardName[INPUT_BUFFER_LIMIT] = {0};
     char encryptedIpAddress[INPUT_BUFFER_LIMIT] = {0};
     char encryptedWifiSSID[INPUT_BUFFER_LIMIT] = {0};
-    gatherAndEncryptBoardInfo(encryptedBoardName, encryptedFirmwareVersion, encryptedIpAddress, encryptedWifiSSID);
+
+    
+    gatherAndEncryptBoardInfo(encryptedFirmwareVersion, encryptedBoardName, encryptedIpAddress, encryptedWifiSSID);
 
     // Create a FirebaseJson object to hold the data
     FirebaseJson json;
@@ -81,20 +109,36 @@ void sendBoardInfo() {
     json.set(boardId + "/ssid", encryptedWifiSSID);
 
     // Send the data to Firebase
-    if (Firebase.updateNode(firebaseData, "boards", json)) {
-        Serial.println("Board info added to Firebase successfully!");
-        boardInfoSent = true; // Set the flag to true to prevent sending again
-    } else {
-        Serial.print("Error adding board info to Firebase: ");
-        Serial.println(firebaseData.errorReason());
-    }
+    sendFirebaseData(json, "boards");
+}
+
+void sendTemperature() {
+  // Get unique identifier (MAC address) for the board
+  String boardId = WiFi.macAddress();
+  
+  float temperature = random(0, 100) + random(0, 99) / 100.0; // Generate a random float between 0 and 100
+  char buffer[20]; // Adjust the buffer size as needed
+  char encryptedTemperature[INPUT_BUFFER_LIMIT] = {0};
+  dtostrf(temperature, 6, 3, buffer);
+
+  // Generate a new IV for each encryption operation
+  byte temp_enc_iv[N_BLOCK];
+  generateNewIV(temp_enc_iv, enc_iv_4);
+
+  encryptAndConvertToHex(buffer, encryptedTemperature, temp_enc_iv);
+
+  // Create a FirebaseJson object to hold the data
+  FirebaseJson json;
+
+  json.set("temperature", encryptedTemperature);
+
+  // Send the data to Firebase
+  String nodePath = "boards/" + boardId;
+  sendFirebaseData(json, nodePath.c_str());
 }
 
 void loop() {
-    // Check if board information has been sent
-    if (!boardInfoSent) {
-        sendBoardInfo();
-    }
+    sendTemperature();
 
     // Rest of your loop code...
 
